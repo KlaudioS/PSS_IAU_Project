d2c5a3c7afbc8375934a92b01ea1e8b9979b469c
Claudio Silva <KlaudioS@Mac-Pro-de-Claudio.local>
2014-11-27 18:46:38 +0000

    Test Commit

--- ProjectoPSS_IAU/FirstViewController.swift
+++ ProjectoPSS_IAU/FirstViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var carroImage: UIImageView!
+    
+    @IBOutlet weak var labelLeftArrow: UILabel!
+    
+    @IBOutlet weak var LabelLeft: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        // Set image over layer
+           }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func buttonClick(sender: AnyObject) {
+        for i in 1...30 {
+            LabelLeft.backgroundColor = UIColorFromRGB(209734)
+        }
+     
+    }
+    
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
 
 }
-
