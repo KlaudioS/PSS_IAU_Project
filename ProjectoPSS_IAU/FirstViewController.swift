c7cefee035eecf639370f24730c7e4865fc713cb
Claudio Silva <KlaudioS@Mac-Pro-de-Claudio.local>
2014-11-28 18:34:20 +0000

    Push Claudio v1

--- ProjectoPSS_IAU/FirstViewController.swift
+++ ProjectoPSS_IAU/FirstViewController.swift
@@ -9,40 +9,33 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    var x:CGFloat = 0
+    
+    @IBOutlet weak var RightColor: UIImageView!
+    
+    @IBOutlet weak var LeftColor: UIImageView!
+    @IBOutlet weak var carColor: UIImageView!
     @IBOutlet weak var carroImage: UIImageView!
-    
-    @IBOutlet weak var labelLeftArrow: UILabel!
-    
-    @IBOutlet weak var LabelLeft: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         // Set image over layer
-           }
-
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     
-    @IBAction func buttonClick(sender: AnyObject) {
-        for i in 1...30 {
-            LabelLeft.backgroundColor = UIColorFromRGB(209734)
-        }
-     
+    func ChangeColor(speed:CGFloat,viewToChange:UIImageView){
+        var vermelho = speed
+        var verde = 255-vermelho
+        let color = UIColor(red: (vermelho/255.0),green: (verde/255.0), blue: (0/255.0), alpha: 1.0)
+        viewToChange.backgroundColor = color
     }
     
-    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
-        return UIColor(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
-        )
-    }
-
 }
