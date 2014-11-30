7ab535b90339036512b6fea77ece2e63e9411446
Admin Administrator <admin@Admins-Mac-Pro.local>
2014-11-30 04:38:29 +0000

    Started testing GPS (I put it temporarily on SecondView)

--- ProjectoPSS_IAU/SecondViewController.swift
+++ ProjectoPSS_IAU/SecondViewController.swift
@@ -24,6 +24,7 @@ class SecondViewController: UIViewController {
             gpsManager.requestWhenInUseAuthorization()
         }
         
+        
     }
 
     override func didReceiveMemoryWarning() {
