f55f453664c97208ea8f4881ed21dce5278c3464
Admin Administrator <admin@Admins-Mac-Pro.local>
2014-11-30 03:17:24 +0000

    Added accelerometer readings and modifying images with that data

--- ProjectoPSS_IAU/FirstViewController.swift
+++ ProjectoPSS_IAU/FirstViewController.swift
@@ -7,21 +7,63 @@
 //
 
 import UIKit
+import CoreMotion
 
 class FirstViewController: UIViewController {
     
+    // Constants
+    let accelerometerUpdateInterval = 0.2
     
     
+    // Accelerometer initialization
+    let motionManager = CMMotionManager()
+    
+    var currentMaxAccelX: Double = 0.0
+    var currentMaxAccelY: Double = 0.0
+    var currentMaxAccelZ: Double = 0.0
+    
+    @IBOutlet weak var accX: UILabel!
+    @IBOutlet weak var accY: UILabel!
+    @IBOutlet weak var accZ: UILabel!
+    
+    @IBOutlet weak var maxAccX: UILabel!
+    @IBOutlet weak var maxAccY: UILabel!
+    @IBOutlet weak var maxAccZ: UILabel!
+    
+    
+    // UI element declarations
     @IBOutlet weak var RightColor: UIImageView!
     @IBOutlet weak var LeftColor: UIImageView!
     @IBOutlet weak var carColor: UIImageView!
+    
+    
   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        // Set image over layer
+        // Check if the accelerometer is inactive and available
+        if self.motionManager.accelerometerActive {
+            self.stopAccelerometer()
+            return
+        }
+        if !self.motionManager.accelerometerAvailable {
+            println("No accelerometer detected.")
+            return
+        }
+        
+        // Run the accelerometer in the background
+        
+        motionManager.accelerometerUpdateInterval = accelerometerUpdateInterval
+
+        
+        motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue(), withHandler: {(accelerometerData: CMAccelerometerData!, error:NSError!)in
+            self.outputAccelerationData(accelerometerData.acceleration)
+            if (error != nil) {
+                println("\(error)")
+            }
+        })
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -30,47 +72,103 @@ class FirstViewController: UIViewController {
         
     }
     
-    @IBAction func ChangeColor(sender: AnyObject) {
-        ChangeColorLeftTurn(100)
-        ChangeColorCarAccelarate(200)
-        ChangeColorRightTurn(300)
+    /*
+     * Accelerometer management functions
+     */
+    
+    // Stops the accelerometer if it's already running
+    func stopAccelerometer () {
+        self.motionManager.stopAccelerometerUpdates()
+        (currentMaxAccelX, currentMaxAccelY, currentMaxAccelZ) = (0, 0, 0)
     }
     
-    func ChangeColorLeftTurn(speed:CGFloat){
-        UIView.animateWithDuration(0.2, animations:{
-            var vermelho:CGFloat = speed*2
-            var verde:CGFloat = 510-vermelho
-            let color = UIColor(red: (vermelho/255.0),green: (verde/255.0), blue: (0/255.0), alpha: 1.0)
+    // Reset accelerometer maximum data
+    @IBAction func resetAccData(sender: AnyObject) {
+        currentMaxAccelX = 0.0
+        currentMaxAccelY = 0.0
+        currentMaxAccelZ = 0.0
+    }
+    
+    // Processes data taken from the accelerometer
+    func outputAccelerationData(acceleration:CMAcceleration) {
+        
+        // Accelerometer data screen display
+        accX.text = NSString(format:"%.4f", acceleration.x)
+        if fabs(acceleration.x) > fabs(currentMaxAccelX) {
+            currentMaxAccelX = acceleration.x
+            maxAccX.text = NSString(format:"%.4f", currentMaxAccelX)
+        }
+        
+        accY.text = NSString(format:"%.4f", acceleration.y)
+        if fabs(acceleration.y) > fabs(currentMaxAccelY) {
+            currentMaxAccelY = acceleration.y
+            maxAccY.text = NSString(format:"%.4f", currentMaxAccelY)
+        }
+        
+        accZ.text = NSString(format:"%.4f", acceleration.z)
+        if fabs(acceleration.z) > fabs(currentMaxAccelZ) {
+            currentMaxAccelZ = acceleration.z
+            maxAccZ.text = NSString(format:"%.4f", currentMaxAccelZ)
+        }
+
+        
+        // Change arrow colors
+        if acceleration.x > Double(0) {
+            ChangeColorRightTurn(CGFloat(acceleration.x))
+        }
+        if acceleration.x <= Double(0) {
+            ChangeColorLeftTurn(CGFloat(abs(acceleration.x)))
+        }
+        
+        // Change car color
+        if acceleration.z > Double(0) {
+            ChangeColorCarBrake(CGFloat(acceleration.z))
+        }
+        if acceleration.z <= Double(0) {
+            ChangeColorCarAccelerate(CGFloat(abs(acceleration.z)))
+        }
+        
+    }
+    
+    @IBAction func ChangeColor(sender: AnyObject) {
+        ChangeColorLeftTurn(0.33)
+        ChangeColorCarAccelerate(0.66)
+        ChangeColorRightTurn(1)
+    }
+    
+    /*
+     *  Changing colors of the interface
+     */
+    func ChangeColorLeftTurn(speed: CGFloat) {
+        UIView.animateWithDuration(accelerometerUpdateInterval, animations:{
+            var red:CGFloat = speed * 2 * 300
+            var green:CGFloat = 510 - red
+            let color = UIColor(red: (red/255.0), green: (green/255.0), blue: (0/255.0), alpha: 1.0)
             self.LeftColor.backgroundColor = color})
     }
-    
-    func ChangeColorRightTurn(speed:CGFloat){
-        UIView.animateWithDuration(0.2, animations:{
-            var vermelho:CGFloat = speed*2
-            var verde:CGFloat = 510-vermelho
-            let color = UIColor(red: (vermelho/255.0),green: (verde/255.0), blue: (0/255.0), alpha: 1.0)
+    func ChangeColorRightTurn(speed: CGFloat) {
+        UIView.animateWithDuration(accelerometerUpdateInterval, animations:{
+            var red:CGFloat = speed * 2 * 300
+            var green:CGFloat = 510 - red
+            let color = UIColor(red: (red/255.0), green: (green/255.0), blue: (0/255.0), alpha: 1.0)
             self.RightColor.backgroundColor = color})
         
     }
-    
-    func ChangeColorCarAccelarate(speed:CGFloat){
-        UIView.animateWithDuration(0.2, animations:{
-            var vermelho:CGFloat = speed*2
-            var verde:CGFloat = 510-vermelho
-            let color = UIColor(red: (vermelho/255.0),green: (verde/255.0), blue: (0/255.0), alpha: 1.0)
+    func ChangeColorCarAccelerate(speed: CGFloat) {
+        UIView.animateWithDuration(accelerometerUpdateInterval, animations:{
+            var red:CGFloat = speed * 2 * 300
+            var green:CGFloat = 510 - red
+            let color = UIColor(red: (red/255.0), green: (green/255.0), blue: (0/255.0), alpha: 1.0)
             self.carColor.backgroundColor = color})
         
     }
-    
-    
-    func ChangeColorCarBreak(speed:CGFloat){
-        UIView.animateWithDuration(0.2, animations:{
-            var vermelho:CGFloat = speed*2
-            var verde:CGFloat = 510-vermelho
-            let color = UIColor(red: (vermelho/255.0),green: (verde/255.0), blue: (0/255.0), alpha: 1.0)
+    func ChangeColorCarBrake(speed: CGFloat) {
+        UIView.animateWithDuration(accelerometerUpdateInterval, animations:{
+            var red:CGFloat = speed * 2 * 300
+            var green:CGFloat = 510 - red
+            let color = UIColor(red: (red/255.0), green: (green/255.0), blue: (0/255.0), alpha: 1.0)
             self.carColor.backgroundColor = color})
         
     }
-
     
 }
